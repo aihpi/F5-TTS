154faeb3e6468cdfce36036f4403302e7d8f24e0
Johanna Reiml <johanna.reiml@hpi.de>
2024-12-02 14:04:53 +0100

    feat: use only remote models and add german vocos

--- src/f5_tts/demo/constants.py
+++ src/f5_tts/demo/constants.py
@@ -1,16 +1,23 @@
 # Define model constants
 MODEL_CHECKPOINTS = {
+    # German vocos checkpoints
+    "kisz-german-vocos-295000": "hf://aihpi/F5-TTS-German/F5TTS_Base/model_295000.safetensors",
     # German bigvgan checkpoints
-    "kisz-german-bigvgan-295000": "/home/johanna.reiml/F5TTS-German/model_295000.pt",
-    "kisz-german-bigvgan-430000": "/home/johanna.reiml/F5TTS-German/model_430000.pt",
-    "kisz-german-bigvgan-550000": "/home/johanna.reiml/F5TTS-German/model_550000.pt",
-    "kisz-german-bigvgan-615000": "/home/johanna.reiml/F5TTS-German/model_615000.pt",
+    "kisz-german-bigvgan-295000": "hf://aihpi/F5-TTS-German/F5TTS_Base_bigvgan/model_295000.safetensors",
+    "kisz-german-bigvgan-430000": "hf://aihpi/F5-TTS-German/F5TTS_Base_bigvgan/model_430000.safetensors",
+    "kisz-german-bigvgan-550000": "hf://aihpi/F5-TTS-German/F5TTS_Base_bigvgan/model_550000.safetensors",
+    "kisz-german-bigvgan-615000": "hf://aihpi/F5-TTS-German/F5TTS_Base_bigvgan/model_615000.safetensors",
     # English checkpoints
     "original-english-vocos": "hf://SWivid/F5-TTS/F5TTS_Base/model_1200000.pt",
     "original-english-bigvgan": "hf://SWivid/F5-TTS/F5TTS_Base_bigvgan/model_1250000.pt"
 }
 
 MODEL_METADATA = {
+    # German vocos checkpoints
+    "kisz-german-vocos-295000": {
+        "vocoder_name": "vocos",
+        "language": "de"
+    },
     # German bigvgan checkpoints
     "kisz-german-bigvgan-295000": {
         "vocoder_name": "bigvgan",
