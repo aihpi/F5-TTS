17f84cb061819bcc41ad20c3661e0df1df556753
Johanna Reiml <johanna.reiml@hpi.de>
2024-12-06 15:54:14 +0100

    fix: keep best german checkpoints

--- src/f5_tts/demo/constants.py
+++ src/f5_tts/demo/constants.py
@@ -1,14 +1,9 @@
 # Define model constants
 MODEL_CHECKPOINTS = {
     # German vocos checkpoints
-    "kisz-german-vocos-295000": "hf://aihpi/F5-TTS-German/F5TTS_Base/model_295000.safetensors",
-    "kisz-german-vocos-365000": "hf://aihpi/F5-TTS-German/F5TTS_Base/model_365000.safetensors",
-    "kisz-german-vocos-420000": "hf://aihpi/F5-TTS-German/F5TTS_Base/model_420000.safetensors",
+    "kisz-german-vocos": "hf://aihpi/F5-TTS-German/F5TTS_Base/model_365000.safetensors",
     # German bigvgan checkpoints
-    "kisz-german-bigvgan-295000": "hf://aihpi/F5-TTS-German/F5TTS_Base_bigvgan/model_295000.safetensors",
-    "kisz-german-bigvgan-430000": "hf://aihpi/F5-TTS-German/F5TTS_Base_bigvgan/model_430000.safetensors",
-    "kisz-german-bigvgan-550000": "hf://aihpi/F5-TTS-German/F5TTS_Base_bigvgan/model_550000.safetensors",
-    "kisz-german-bigvgan-615000": "hf://aihpi/F5-TTS-German/F5TTS_Base_bigvgan/model_615000.safetensors",
+    "kisz-german-bigvgan": "hf://aihpi/F5-TTS-German/F5TTS_Base_bigvgan/model_430000.safetensors",
     # English checkpoints
     "original-english-vocos": "hf://SWivid/F5-TTS/F5TTS_Base/model_1200000.pt",
     "original-english-bigvgan": "hf://SWivid/F5-TTS/F5TTS_Base_bigvgan/model_1250000.pt"
@@ -16,32 +11,12 @@ MODEL_CHECKPOINTS = {
 
 MODEL_METADATA = {
     # German vocos checkpoints
-    "kisz-german-vocos-295000": {
-        "vocoder_name": "vocos",
-        "language": "de"
-    },
-    "kisz-german-vocos-365000": {
-        "vocoder_name": "vocos",
-        "language": "de"
-    },
-    "kisz-german-vocos-420000": {
+    "kisz-german-vocos": {
         "vocoder_name": "vocos",
         "language": "de"
     },
     # German bigvgan checkpoints
-    "kisz-german-bigvgan-295000": {
-        "vocoder_name": "bigvgan",
-        "language": "de"
-    },
-    "kisz-german-bigvgan-430000": {
-        "vocoder_name": "bigvgan",
-        "language": "de"
-    },
-    "kisz-german-bigvgan-550000": {
-        "vocoder_name": "bigvgan",
-        "language": "de"
-    },
-    "kisz-german-bigvgan-615000": {
+    "kisz-german-bigvgan": {
         "vocoder_name": "bigvgan",
         "language": "de"
     },
